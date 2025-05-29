bb4efa32e441f9cb0f30c85d13a0c5027153288e
Nicolas20006 <nicolasmosquera@unicomfacauca.edu.co>
2025-05-29 16:16:25 -0500

    Update FinanzasService.java
    Actulizacion FinanzasService

--- src/main/java/rmi/FinanzasService.java
+++ src/main/java/rmi/FinanzasService.java
@@ -1,14 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rmi;
 
 import java.rmi.Remote;
-/**
- *
- * @author 1
- */
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
 
 public interface FinanzasService extends Remote {
+    String registrarPago(String concepto, double monto) throws RemoteException;
+    Map<String, Double> obtenerPagos() throws RemoteException;
+    double obtenerBalance() throws RemoteException;
 }
