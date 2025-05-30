93898fbba1d6920b7bb95150bc76ac781d2679e0
Dayro18 <dayroaguilar@unicomfacauca.edu.co>
2025-05-30 11:27:08 -0500

    Update FinanzasService.java
    Cambios CitaService

--- src/main/java/rmi/FinanzasService.java
+++ src/main/java/rmi/FinanzasService.java
@@ -1,21 +1,12 @@
-
-/**
- *
- * @Emily Villa
- */
-
 package rmi;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 
 public interface FinanzasService extends Remote {
-    String registrarPago(String cedula, String concepto, double monto) throws RemoteException;
-    String generarReporteDiario() throws RemoteException;
-    String generarReporteMensual() throws RemoteException;
-    List<String> pagosPorMascota(String nombreMascota) throws RemoteException;
-    String balanceGeneral() throws RemoteException;
+    String registrarPago(String concepto, double monto) throws RemoteException;
+    Map<String, Double> obtenerPagos() throws RemoteException;
+    double obtenerBalance() throws RemoteException;
 }
-
-
