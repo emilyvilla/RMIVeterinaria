3fa6b2aa5fa68a62f7bff370081f1221d27dff87
Dayro18 <dayroaguilar@unicomfacauca.edu.co>
2025-05-30 11:25:27 -0500

    Update AdopcionService.java
    Cambios AdopcionService

--- src/main/java/rmi/AdopcionService.java
+++ src/main/java/rmi/AdopcionService.java
@@ -1,17 +1,8 @@
-
-/**
- *
- * @Emily Villa
- */
 package rmi;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
 
 public interface AdopcionService extends Remote {
-    String registrarMascotaAdopcion(String id, String nombre, String estado) throws RemoteException;
-    List<String> listarMascotasAdopcion() throws RemoteException;
-    String cambiarEstadoMascota(String id, String nuevoEstado) throws RemoteException;
-    List<String> historialAdopciones(String cedula) throws RemoteException;
+    String registrarAdopcion(String nombreMascota) throws RemoteException;
 }
